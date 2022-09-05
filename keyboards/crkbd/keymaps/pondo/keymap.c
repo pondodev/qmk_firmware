ba628513b52a18a8fcab612867a44423b83a9152
Daniel Coady <me@pondo.dev>
2022-09-06 09:22:24 +1000

    corne bongo go brr

--- keyboards/crkbd/keymaps/pondo/keymap.c
+++ keyboards/crkbd/keymaps/pondo/keymap.c
@@ -120,14 +120,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 char wpm_str[10];
 // WPM-responsive animation stuff here
 #    define IDLE_FRAMES 5
-#    define IDLE_SPEED 20  // below this wpm value your animation will idle
+#    define IDLE_SPEED 10  // below this wpm value your animation will idle
 
 // #define PREP_FRAMES 1 // uncomment if >1
 
 #    define TAP_FRAMES 2
-#    define TAP_SPEED 40  // above this wpm value typing animation to trigger
+#    define TAP_SPEED 20  // above this wpm value typing animation to trigger
 
-#    define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
+#    define ANIM_FRAME_DURATION 50  // how long each frame lasts in ms
 // #define SLEEP_TIMER 60000 // should sleep after this period of 0 wpm, needs fixing
 #    define ANIM_SIZE 636  // number of bytes in array, minimize for adequate firmware size, max is 1024
 
